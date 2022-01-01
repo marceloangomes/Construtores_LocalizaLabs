1dd22a153b1f9db20dbe29dc1cffaee0abf274b3
marceloangomes <marcelogomes723@gmail.com>
2022-01-01 15:19:40 -0300

    Inicial

---
+++ ExemploConstrutores/Model/Log.cs
@@ -0,0 +1,20 @@
+namespace ExemploConstrutores.Model
+{
+    public class Log
+    {
+        private static Log _log;
+
+        public string PropriedadeLog { get; set; }
+        private Log()
+        {
+            
+        }
+
+        public static Log GetInstance(){
+            if(_log == null){
+                _log = new Log();
+            }
+            return _log;
+        }
+    }
+}
---
+++ ExemploConstrutores/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ExemploConstrutores.Model;
+
+Pessoa p1 = new Pessoa("Patricia", "Gomes");
+
+p1.Apresentar();
+
+var log1 = Log.GetInstance();
+log1.PropriedadeLog = "Somente uma vez";
+var log2 =Log.GetInstance();
+System.Console.WriteLine(log2.PropriedadeLog);
+
+Aluno a1 = new Aluno("Marcelo", "Gomes", "Matematica");
+a1.Apresentar();
+
+
+
+var op = new Operacao(Calculadora.Somar);
+op+=Calculadora.Subtrair;
+op.Invoke(10,10);
+
+Matematica m = new Matematica(30,20);
+m.Somar();
+
+public delegate void Operacao(int x, int y);
+
+
+
+
+
+
+
+
+
+
